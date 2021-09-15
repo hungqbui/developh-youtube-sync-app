9605e64521e080c4980e5824f7d67b925e6658a0
hungbui2004 <quanghungbui8104@gmail.com>
2021-09-15 22:15:28 +0700

    Added time slider

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import express from 'express'
 import path from 'path'
+
 const __dirname = path.resolve()
 
 express()
   .use(express.static(path.join(__dirname, 'src')))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('index'))
-  .listen(3000, () => console.log(`Listening on 3000`))
+  .listen(5500, () => console.log(`Listening on 5500`))
 
 
 
