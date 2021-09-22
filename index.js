1d8037b7ee9195e6a7bb403440e1a65f1e2c1130
hungbui2004 <quanghungbui8104@gmail.com>
2021-09-22 22:02:31 +0700

    Added syncing feature with Socket.io, minor adjustments to code and styling

--- index.js
+++ index.js
@@ -1,15 +1,48 @@
 import express from 'express'
-import path from 'path'
-
 const app = express()
 
+import http from 'http'
+const server = http.createServer(app)
+
+import { Server, Socket } from 'socket.io'
+const io = new Server(server)
+
+import path from 'path'
 const __dirname = path.resolve()
 
-express()
-  .use(express.static(path.join(__dirname, 'src')))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('index'))
-  .listen(5500, () => console.log(`Listening on 5500`))
+app.use(express.static(path.join(__dirname, 'src')))
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => res.render('index'))
+
+
+io.on('connection', (socket)=> {
+  console.log('user đã connect')
+  socket.on('submit-link', (data)=> {
+    console.log(data.message)
+    io.emit('broadcasted-link', data.message)
+  })
+  socket.on('update',()=> {
+    io.emit('update-player')
+  })
+  socket.on('play-clicked',()=> {
+    io.emit('play')
+  })
+  socket.on('pause-clicked',()=> {
+    io.emit('pause')
+  })
+  socket.on('change-time-stamp', (timestamp)=> {
+    io.emit('go-to-time-stamp', timestamp.message)
+  })
+  socket.on('mouse-up', ()=>{
+    io.emit('bar-mouse-up')
+  })
+})
+
+
+
+server.listen(5500, ()=> {
+  console.log('Listening on 5500')
+})
 
 
 
